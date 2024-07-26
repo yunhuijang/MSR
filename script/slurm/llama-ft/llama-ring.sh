846fa96996c9aff5b376cd799b56b8ead56335f0
yunhuijang <uni551000@gmail.com>
2024-07-26 15:15:06 +0900

    Fix script

--- script/slurm/llama-ft/llama-ring.sh
+++ script/slurm/llama-ft/llama-ring.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #SBATCH -J ft-llama-ring
-#SBATCH -p A100-80GB
+#SBATCH -p A5000
 #SBATCH --gres=gpu:4
 #SBATCH -o sbatch_log/%x.out
 
@@ -25,10 +25,11 @@ srun python model/one_stage_generator_llama.py \
 --cot_mode_multiset None \
 --cot_mode_ring \
 --wandb_mode online \
---train_batch_size 2 \
---eval_batch_size 2 \
+--train_batch_size 256 \
+--eval_batch_size 4 \
+--gen_batch_size 64 \
 --epochs 250 \
---max_length 1688 \
+--max_length 1024 \
 --run_id 0q8vv7pn
 
 
