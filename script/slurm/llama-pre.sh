6b0b373a6a222ef6cfccd08aea78f2d00f2fa5b3
yunhuijang <uni551000@gmail.com>
2024-07-30 22:59:14 +0900

    Add llama pre script

---
+++ script/slurm/llama-pre.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#SBATCH -J llama-pre
+#SBATCH -p A100-80GB
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python model/pretrain_llama.py \
+--architecture llama \
+--wandb_mode online \
+--train_batch_size 4 \
+--eval_batch_size 4 \
+--gen_batch_size 32 \
+--epochs 10 \
+--max_length 100
+
+
+
