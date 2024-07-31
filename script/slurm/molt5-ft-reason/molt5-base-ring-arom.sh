e2a94078d0efa0fdb997d99a6c19966ab59b255e
yunhuijang <uni551000@gmail.com>
2024-07-31 22:59:46 +0900

    Add aromatic script

---
+++ script/slurm/molt5-ft-reason/molt5-base-ring-arom.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#SBATCH -J reason-base-ra
+#SBATCH -p A6000
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python model/reasoning_generator.py \
+--architecture molt5-base \
+--cot_mode_multiset None \
+--cot_mode_ring \
+--cot_mode_aromatic \
+--wandb_mode online \
+--train_batch_size 8 \
+--eval_batch_size 8 \
+--epochs 250 \
+--max_length 512
+
+
+
