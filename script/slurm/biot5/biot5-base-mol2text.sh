f44da302ccca6a61ee60561e9d0314af06a76f95
yunhuijang <uni551000@gmail.com>
2024-08-09 17:06:25 +0900

    Add bioT5 pretrain script

---
+++ script/slurm/biot5/biot5-base-mol2text.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#SBATCH -J biot5-base-mol2text
+#SBATCH -p A6000
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python evaluate_pretrained_model.py \
+--architecture biot5-base \
+--split test \
+--model_id QizhiPei \
+--finetune_task -text2mol \
+--wandb_mode online 
+
