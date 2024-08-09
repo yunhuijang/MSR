59de4d7887c992dc79c09e159d6a3d3d5ba290cc
yunhuijang <uni551000@gmail.com>
2024-08-09 17:50:05 +0900

    Add BioT5 script

---
+++ script/slurm/biot5-ft-reason/biot5-base-type.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#SBATCH -J reason-biobase-type
+#SBATCH -p A6000
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python model/one_stage_generator.py \
+--architecture biot5-plus-base \
+--cot_mode_multiset only_type \
+--wandb_mode online \
+--train_batch_size 8 \
+--eval_batch_size 8 \
+--epochs 250
+
+
+
---
+++ script/slurm/biot5-ft/biot5-large-car.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#SBATCH -J ft-biolarge-car
+#SBATCH -p A100-80GB
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python model/one_stage_generator.py \
+--architecture biot5-plus-large \
+--cot_mode_multiset None \
+--cot_mode_aromatic \
+--cot_mode_ring \
+--cot_mode_chain \
+--wandb_mode online \
+--train_batch_size 8 \
+--eval_batch_size 8 \
+--epochs 250 \
+--run_id 1xs50x69
+
+
+
