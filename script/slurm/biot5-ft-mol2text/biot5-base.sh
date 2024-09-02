72c1e3066c9e0bf6933ecdc2cdef3e0ac9aafa9f
yunhuijang <uni551000@gmail.com>
2024-09-02 17:18:06 +0900

    Add BioT5 mol2text script

---
+++ script/slurm/biot5-ft-mol2text/biot5-base.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#SBATCH -J ft-biobase
+#SBATCH -p A6000
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python model/one_stage_generator.py \
+--architecture biot5-plus-base \
+--cot_mode_multiset None \
+--wandb_mode online \
+--train_batch_size 8 \
+--eval_batch_size 8 \
+--epochs 250 \
+--model_id QizhiPei \
+--run_id bh29l5sd
+
+
+
+
