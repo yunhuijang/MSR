a3eca53ac9492a101a951fd589c70c144ba324e1
yunhuijang <uni551000@gmail.com>
2024-09-08 02:24:11 +0900

    Add analysis script

---
+++ script/generalist/analysis.sh
@@ -0,0 +1,6 @@
+python model/analyze_structure_failure.py \
+    --wandb_mode online \
+    --cot_mode_aromatic \
+    --cot_mode_chain \
+    --cot_mode_con_ring_name \
+    --cot_mode_functional_group
