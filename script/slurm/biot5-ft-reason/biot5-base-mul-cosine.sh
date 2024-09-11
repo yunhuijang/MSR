283f2876969b499cd87fd38173fd498a6f89d538
yunhuijang <uni551000@gmail.com>
2024-09-11 14:53:24 +0900

    Fix script

--- script/slurm/biot5-ft-reason/biot5-base-mul-cosine.sh
+++ script/slurm/biot5-ft-reason/biot5-base-mul-cosine.sh
@@ -38,7 +38,8 @@ srun python model/reasoning_generator.py \
 --warmup_ratio 0.1 \
 --check_val_every_n_epoch 5 \
 --lr_scheduler_type cosine \
---max_length 820
+--max_length 820 \
+--run_id ro68ks2b
 
 
 
