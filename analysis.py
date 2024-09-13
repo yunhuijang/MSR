9757d5b2381cd65a066243df29e78db3c0162604
yunhuijang <uni551000@gmail.com>
2024-09-14 01:15:01 +0900

    Fix error

--- analysis.py
+++ analysis.py
@@ -195,6 +195,7 @@ def map_functional_group_from_cot(cot):
     if "The functional groups present in the molecule include" in cot:
         functional_groups = cot[len(" The functional groups present in the molecule include "):-1]
         functional_groups = functional_groups.replace('and', '')
+        functional_groups = functional_groups.replace('groups', '')
         fgs = set(functional_groups.split(','))
         fgs = [fg.strip() for fg in fgs]
     else:
