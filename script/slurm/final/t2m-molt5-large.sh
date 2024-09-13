65e6e0b4ed268f4a8fa43dd438cfae0c1b3d7d58
yunhuijang <uni551000@gmail.com>
2024-09-13 22:55:10 +0900

    Fix script

--- script/slurm/final/t2m-molt5-large.sh
+++ script/slurm/final/t2m-molt5-large.sh
@@ -25,8 +25,8 @@ srun python model/one_stage_generator.py \
 --architecture molt5-large \
 --cot_mode multiset_formula-chain-aromatic-con_ring_name-func_simple \
 --wandb_mode online \
---train_batch_size 8 \
---eval_batch_size 8 \
+--train_batch_size 4 \
+--eval_batch_size 4 \
 --epochs 250 \
 --model_id laituan245 \
 --max_length 820 \
