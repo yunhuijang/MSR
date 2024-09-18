eeeb2c7b1d688df0c581888d8536546e7824714d
yunhuijang <uni551000@gmail.com>
2024-09-18 15:34:47 +0900

    Fix script

--- script/slurm/final/m2t-biot5-org-base-van.sh
+++ script/slurm/final/m2t-biot5-org-base-van.sh
@@ -2,7 +2,8 @@
 
 #SBATCH -J biobase-org-final-m2t-van
 #SBATCH --exclude=n76,n56,n54,n79
-#SBATCH -p A6000
+#SBATCH -p A100-80GB
+#SBATCH -q add_hpgpu
 #SBATCH --gres=gpu:4
 #SBATCH -o sbatch_log/%x.out
 
