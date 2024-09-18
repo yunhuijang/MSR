643adbee0716498b12fc25b39e8a97fbfbe711fe
yunhuijang <uni551000@gmail.com>
2024-09-18 10:50:54 +0900

    Fix script

---
+++ script/slurm/final/t2m-molt5-large-chiral.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#SBATCH -J mollarge-final-t2m-chiral
+#SBATCH --exclude=n76,n56,n54,n79
+#SBATCH -p A100-80GB
+#SBATCH -q add_hpgpu
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python model/one_stage_generator.py \
+--architecture molt5-large \
+--cot_mode multiset_formula-chain-aromatic-con_ring_name-func_simple-chiral \
+--wandb_mode online \
+--train_batch_size 4 \
+--eval_batch_size 4 \
+--epochs 250 \
+--model_id laituan245 \
+--max_length 820 \
+--generation_mode \
+--max_new_tokens 768 \
+--check_val_every_n_epoch 10
+
+
+
+
