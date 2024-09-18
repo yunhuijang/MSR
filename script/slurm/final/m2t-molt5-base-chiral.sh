9d505f5409bd4dd9ed2a5f85fc0d035f9ee1ca8e
yunhuijang <uni551000@gmail.com>
2024-09-18 10:48:14 +0900

    Fix script

--- script/slurm/final/m2t-molt5-base-chiral.sh
+++ script/slurm/final/m2t-molt5-base-chiral.sh
@@ -33,7 +33,8 @@ srun python model/one_stage_generator_mol2text.py \
 --max_length 820 \
 --generation_mode \
 --max_new_tokens 512 \
---check_val_every_n_epoch 5
+--check_val_every_n_epoch 5 \
+--run_id 2nhg923r
 
 
 
