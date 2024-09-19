7463431cd30c26bc78ecf92dd57acc2cf852dc47
yunhuijang <uni551000@gmail.com>
2024-09-19 13:04:22 +0900

    Fix error

--- util.py
+++ util.py
@@ -1,9 +1,17 @@
 import selfies
 
 def selfies_to_smiles(sf):
+    sf = sf.replace('<bom>', '')
+    sf = sf.replace('<eom>', '')
     try:
         smiles = selfies.decoder(sf)
     except:
         smiles = ""
     return smiles
-        
+
+def smiles_to_selfies(smi):
+    try:
+        sf = selfies.encoder(smi)
+    except:
+        sf = ""
+    return sf
