bb5d1f037fe5e27112696402eb8911d8e5aab343
yunhuijang <uni551000@gmail.com>
2024-09-20 14:09:06 +0900

    Add script

---
+++ script/slurm/real_final/m2t-molt5-base-chem.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#SBATCH -J m2t-molt5-base-chem
+#SBATCH --exclude=n76,n56,n54,n52
+#SBATCH -p A100-80GB
+#SBATCH -q add_hpgpu
+#SBATCH --gres=gpu:4
+#SBATCH -o sbatch_log/%x.out
+
+cd $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+date
+
+nvidia-smi
+
+srun python model/one_stage_generator_mol2text.py \
+--architecture molt5-base \
+--cot_mode name-weight-func_chem \
+--wandb_mode online \
+--train_batch_size 8 \
+--eval_batch_size 8 \
+--epochs 250 \
+--model_id laituan245 \
+--max_length 820 \
+--generation_mode \
+--max_new_tokens 512 \
+--check_val_every_n_epoch 20
+
+
+
+
