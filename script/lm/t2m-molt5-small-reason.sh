d83408edd9a1c678aed2e272c717c8348d68bc7b
yunhuijang <uni551000@gmail.com>
2025-01-13 09:33:55 +0900

    Fix script

--- script/lm/t2m-molt5-small-reason.sh
+++ script/lm/t2m-molt5-small-reason.sh
@@ -25,11 +25,11 @@ nvidia-smi
 
 srun python model/reasoning_generator.py \
 --architecture molt5-small \
---cot_mode multiset_formula-chain-aromatic-con_ring_name-func_simple-chiral \
+--cot_mode multiset_formula-chain-aromatic-con_ring_name-func_simple-chiral-weight-name \
 --wandb_mode online \
 --train_batch_size 8 \
 --eval_batch_size 8 \
---epochs 250 \
+--epochs 100 \
 --model_id laituan245 \
 --max_length 820 \
 --generation_mode \
